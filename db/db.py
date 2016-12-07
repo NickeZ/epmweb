5e5073cff4c1a2d6605d4f853d210182aa14e614
Niklas Claesson <nicke.claesson@gmail.com>
2016-12-07 23:10:45 +0100

    wip

--- db/db.py
+++ db/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, text
 
 Base = declarative_base()
 
@@ -7,11 +7,11 @@ class Package(Base):
     __tablename__ = 'packages'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    updated = Column(DateTime)
-    created = Column(DateTime)
-    max_version = Column(String)
-    downloads = Column(Integer)
+    name = Column(String, unique=True, nullable=False)
+    updated = Column(DateTime, nullable=False)
+    created = Column(DateTime, nullable=False, server_default=text('NOW()'))
+    max_version = Column(String, nullable=False)
+    downloads = Column(Integer, nullable=False)
     description = Column(String)
     repository = Column(String)
 
@@ -19,11 +19,10 @@ class Versions(Base):
     __tablename__ = 'versions'
 
     id = Column(Integer, primary_key=True)
-    version = Column(String)
+    version = Column(String, nullable=False)
 
 class Users(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String)
-
+    email = Column(String, nullable=False)
